0ef4d272eb37b14726bd8cbf3ceb051667ec43c8
elissamim <elisamim@gmail.com>
2025-02-20 15:41:18 +0000

    (add) init package

---
+++ src/industrial_economics/__init__.py
@@ -0,0 +1,3 @@
+from .market_concentration_indicators import (gini_index, lorenz_curve, 
+                                              hhi, concentration_ratio, 
+                                              shannon_entropy, theil_index)
